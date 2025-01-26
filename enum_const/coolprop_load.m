63781aee092c9cba5c09f1e8eb13cba42667b3c1
xdthegreat <46622530+xdthegreat@users.noreply.github.com>
2025-01-26 15:33:52 +0000

    pump added and fixed

--- enum_const/coolprop_load.m
+++ enum_const/coolprop_load.m
@@ -5,16 +5,13 @@ system([e,' -m pip install --user -U CoolProp']);
 
 
 % http://www.coolprop.org/coolprop/HighLevelAPI.html#parameter-table
-py.CoolProp.CoolProp.PropsSI('D','P',101325,'T',300,'Hydrogen');  %get density from pressure and temperature
+pressure_vector = 100000:10000:1000000;
+
+u_min_liquid = py.CoolProp.CoolProp.PropsSI('Umass','P|liquid',min(pressure_vector),'T',15,'Hydrogen');  %get internal energy from pressure and temperature
+u_max_gas = py.CoolProp.CoolProp.PropsSI('Umass','P',max(pressure_vector),'T',400,'Hydrogen');  %get internal energy from pressure and temperature
 
 
-pressure_vector = 100000:10000:700000;
-
-u_min_liquid = py.CoolProp.CoolProp.PropsSI('Umass','P',min(pressure_vector),'T',15,'Hydrogen');  %get internal energy from pressure and temperature
-u_max_gas = py.CoolProp.CoolProp.PropsSI('Umass','P',max(pressure_vector),'T',300,'Hydrogen');  %get internal energy from pressure and temperature
-
-
-H2Tables = twoPhaseFluidTables([-1.45*10,3*10^3],[0.10,1],100,100,100,...
+H2Tables = twoPhaseFluidTables([u_min_liquid/1000,u_max_gas/1000],[0.10,1],100,1000,100,...
 'Hydrogen','py.CoolProp.CoolProp.PropsSI');
 
 save('H2Tables.mat', 'H2Tables')
--- full model/design_details.m
+++ full model/design_details.m
@@ -1,15 +1,19 @@
+%% TODO list:
+% figure out tank heat transfer properties
 
-
-Required_energy = 2667;  % MJ
+Required_energy = 1000;  % MJ
+Equivalent_battery_mass = Required_energy*10^6/(400*3600);
 Generator_efficiency = 0.3;
 m_LH2 = Required_energy/H2_energy_density/Generator_efficiency;
 
 % tank_size
-AC_tank_volume = m_LH2/LH2_density/0.8;
+AC_tank_vol_limit = 0.8;
+AC_tank_volume = m_LH2/LH2_density/AC_tank_vol_limit;
 AC_tank_radius = 0.5;
 AC_tank_cross_sectional_area = AC_tank_radius^2*pi;
 AC_tank_surface_area = AC_tank_cross_sectional_area*2 + AC_tank_radius*2*pi*AC_tank_volume/AC_tank_cross_sectional_area;
-AC_wall_thickness = 5/1000;
+AC_wall_thickness = 2/1000;
+AC_tank_weight = AC_wall_thickness*AC_tank_surface_area*steel_density*2;
 
 % refuel volumetric flow rates
 refuel_time = 3; %mins
@@ -44,3 +48,6 @@ hose_length = 20;  %m
 
 hose_thermal_conductivity = 0.01;  %W/(m*K)
 pipe_thermal_conductivity = 0.005;  %W/(m*K)
+
+%Controller info
+timestep = 0.5;
