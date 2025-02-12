2a4a103f88bed8063e9b8e3929cd19d47454bf2e
xdthegreat <46622530+xdthegreat@users.noreply.github.com>
2025-02-12 11:20:08 +0000

    Added FMU for backwards compatability

--- Sensitivity_sweep/R2024a_test.m
+++ Sensitivity_sweep/R2024a_test.m
@@ -13,15 +13,16 @@ rapid_flag = false;
 tic;
 
 %parsim version
-mdl = "simscape_automatic_R2024a";
+mdl = "simscape_automatic_FMU_R2024a";
 simIn(1:length(valve_diameter_vector)) = Simulink.SimulationInput(mdl); 
 for i = 1:length(valve_diameter_vector) 
-    if rapid_flag
-        simIn(i) = simIn(i).setModelParameter(SimulationMode="rapid-accelerator");
-        simIn(i) = simIn(i).setModelParameter(RapidAcceleratorUpToDateCheck="off");
-    else
-        simIn(i) = simIn(i).setModelParameter('SimulationMode','accelerator');
-    end
+
+    % if rapid_flag
+    %     simIn(i) = simIn(i).setModelParameter(SimulationMode="rapid-accelerator");
+    %     simIn(i) = simIn(i).setModelParameter(RapidAcceleratorUpToDateCheck="off");
+    % else
+    %     simIn(i) = simIn(i).setModelParameter('SimulationMode','accelerator');
+    % end
 
     AC_return_valve_inner_diameter = valve_diameter_vector(i)*2;
     AC_return_valve_orifice_area = AC_return_valve_inner_diameter^2*pi;
