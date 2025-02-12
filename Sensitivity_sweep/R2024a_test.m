4e8bc14b58fe34d542e95dfc0f7e09b4087dd060
xdthegreat <46622530+xdthegreat@users.noreply.github.com>
2025-02-12 13:33:30 +0000

    Updated R2024a models

--- Sensitivity_sweep/R2024a_test.m
+++ Sensitivity_sweep/R2024a_test.m
@@ -9,20 +9,21 @@ clc
 % This changes aircraft valve orifice area over a variety of values
 
 valve_diameter_vector = 0.01:0.002:0.024;
-rapid_flag = false;
+rapid_flag = true;
 tic;
 
 %parsim version
 mdl = "simscape_automatic_R2024a";
+Simulink.BlockDiagram.buildRapidAcceleratorTarget(mdl);
 simIn(1:length(valve_diameter_vector)) = Simulink.SimulationInput(mdl); 
 for i = 1:length(valve_diameter_vector) 
 
-    % if rapid_flag
-    %     simIn(i) = simIn(i).setModelParameter(SimulationMode="rapid-accelerator");
-    %     simIn(i) = simIn(i).setModelParameter(RapidAcceleratorUpToDateCheck="off");
-    % else
-    %     simIn(i) = simIn(i).setModelParameter('SimulationMode','accelerator');
-    % end
+    if rapid_flag
+        simIn(i) = simIn(i).setModelParameter(SimulationMode="rapid-accelerator");
+        simIn(i) = simIn(i).setModelParameter(RapidAcceleratorUpToDateCheck="off");
+    else
+        simIn(i) = simIn(i).setModelParameter('SimulationMode','accelerator');
+    end
 
     AC_return_valve_inner_diameter = valve_diameter_vector(i)*2;
     AC_return_valve_orifice_area = AC_return_valve_inner_diameter^2*pi;
